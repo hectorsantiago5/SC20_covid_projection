502185244f01c26faa1ebae9c01f461d18d1e623
J4yM1l <jchrismilton@gmail.com>
2020-11-09 03:40:38 -0500

    added covid data table

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import csv
 import requests
 import pandas as pd
 
-from flask import Flask, render_template  # request redirect, flash, url_for
+from flask import Flask, render_template, request #redirect, flash, url_for
+
 
 app = Flask(__name__)
 
@@ -33,21 +34,29 @@ def aboutus():
     return render_template('aboutus.html')
 
 
+# @app.route('/covid',methods = ['POST', 'GET'])
+# def covid():
+#     if request.method == 'POST':
+#       result = request.form
+    
+#     return render_template("covid.html", result=result)
+
 # Function to get a COVID-19 data file and display the contents of it
-@app.route('/covid')
+@app.route('/covid',methods = ['POST', 'GET'])
 def covid():
-    # with open('covid_file/us-counties.csv', 'r') as covidfile:
-    #     csv_read = csv.DictReader(covidfile)
-    #     # print(csv_read)
-    #     covid_list = []
+    with open('us-counties.csv', 'r') as covidfile:
+        csv_read = csv.DictReader(covidfile)
+        # print(csv_read)
+        covid_list = []
     #
     #     # For loop to get the last 7 days of Covid Data
-    #     for j in list(reversed(list(csv_read)))[0:7]:
-    #         # print(j)
-    #         date = j['date']
-    #         cases = j['cases']
-    #         deaths = j['deaths']
-    #         covid_list.append({'date': date, 'cases': cases, 'deaths': deaths})
+        for j in list(reversed(list(csv_read)))[0:7]:
+            # print(j)
+            date = j['date']
+            cases = j['cases']
+            deaths = j['deaths']
+            covid_list.append({'date': date, 'cases': cases, 'deaths': deaths})
+        # print(covid_list)
     #
     #     # Function to compute and display the COVID-19 death rate
     #     def compute_csv():
@@ -61,8 +70,8 @@ def covid():
     #                 rate = int(deaths) / int(cases)
     #                 data.append({'cases': cases, 'deaths': deaths, 'rate': rate})
     #                 return data
-
-    return render_template('covid.html')  # , l=covid_list, k=compute_csv())
+# covid()
+    return render_template('covid.html', covid_list=covid_list)  # , l=covid_list, k=compute_csv())
 
 
 app.secret_key = 'some_secret_key_27'
